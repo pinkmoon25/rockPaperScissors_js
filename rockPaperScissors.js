df29771a81db6fa7adafc6e5cb3e32e5537d21da
Tushar <singhtushar348@gmail.com>
2022-04-08 02:32:20 +0530

    Added Ui to the Game

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,107 +1,118 @@
+let playerScore = 0;
+
+let computerScore = 0;
+
+const rockBtn = document.querySelector(".rock");
+
+const paperBtn = document.querySelector(".paper");
+
+const scissorBtn = document.querySelector(".scissor");
+
+const humanScore = document.querySelector(".h-runningscore");
+
+const compScore = document.querySelector(".c-runningscore");
+
+const result = document.querySelector(".result>p")
+
+
 function computerPlay(){
-    
     let num = Math.floor(Math.random() *3);
-
-    if(num === 0){
-        return "Rock";
+    switch(num){
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "paper";
+            break;
+        case 2:
+            return "scissor";
+            break;
     }
-    else if (num === 1){
-        return "Paper";
-    }
-    else{
-        return "Scissors";
-    }
-
 }
 
-function playRound(playerSelection, computerSelection){
-
-    playerSelection = 'rock';
-    computerSelection = computerPlay();
-
-    if(playerSelection == "rock" && computerSelection == "Paper"){
-        console.log('lose')//return "lose";
-        console.log("playerSewlection", playerSelection);
-        console.log("computerSelection", computerSelection);
-        
-    }
-    else if(playerSelection == "rock" && computerSelection == "Scissors"){
-        console.log('win')//return "win";
-        console.log("playerSewlection", playerSelection);
-        console.log("computerSelection", computerSelection);
-        
-    }
-    else if(playerSelection == "paper" && computerSelection == "Rock"){
-        console.log('win')//return "win";
-        console.log("playerSewlection", playerSelection);
-        console.log("computerSelection", computerSelection);
-        
-    }
-    else if(playerSelection == "paper" && computerSelection == "Scissors"){
-        console.log('lose')//return "lose";
-        console.log("playerSewlection", playerSelection);
-        console.log("computerSelection", computerSelection);
-        
-    }
-    else if(playerSelection == "scissors" && computerSelection == "Paper"){
-        console.log('win')//return "win";
-        console.log("playerSewlection", playerSelection);
-        console.log("computerSelection", computerSelection);
-        
-    }
-    else if(playerSelection == "scissors" && computerSelection == "Rock"){
-        console.log('lose')//return "lose";
-        console.log("playerSewlection", playerSelection);
-        console.log("computerSelection", computerSelection);
-        
-    }
-    else if(playerSelection == computerSelection.toLowerCase()){
-        console.log('tie')//return "tie";
-        console.log("playerSewlection", playerSelection);
-        console.log("computerSelection", computerSelection);
-    }
-
-}
-
-function game(){
-
-    let playerScore = 0;
-    let computerScore = 0;
-
+function game(playerSelection){
     
-
-    for(let i =0; i<5; i++){
-
-        const computerSelection = computerPlay();
-
-        const playerSelection = window.prompt("Enter your Choice", "rock, paper or scissors").toLowerCase();
-
-        playRound(playerSelection, computerSelection);
-        
-        console.log(playRound(playerSelection, computerSelection));
-
-        if(playRound(playerSelection, computerSelection) == "win"){
-            playerScore += 1;
-        }
-        else if (playRound(playerSelection, computerSelection) == "lose"){
-            computerScore += 1;
-        }       
-   } console.log(playerScore, computerScore);
-
-
-   if(playerScore > computerScore){
-    console.log("You Win!") ;
+    const computerSelection = computerPlay();
+    switch(`${playerSelection}, ${computerSelection}`){
+        case `rock, scissor`:
+        case `scissor, paper`:
+        case `paper, rock`:
+            result.innerText = `human chose ${playerSelection} & beats ${computerSelection}`;
+            return "WIN";
+            break;
+        case `rock, paper`:
+        case `paper, scissor`:
+        case `scissor, rock`:
+            result.innerText = `human chose ${playerSelection} & lost to ${computerSelection}`;
+            return "LOSE";
+            break;
+        case `rock, rock`:
+        case `paper, paper`:
+        case `scissor, scissor`:
+            result.innerText = `human chose ${playerSelection} same as ${computerSelection}`;
+            return "DRAW";
+            break;
     }
-    else if(computerScore > playerScore){
-    console.log("You lose!");
-    }
-    else{
-    console.log("It's a tie");
-    }    
+
 }
 
+function updatescore(playerSelection){
+
+    switch(game(playerSelection)){
+        case "WIN":
+            playerScore++;
+            humanScore.innerText = playerScore;
+            compScore.innerText = computerScore;
+            break;
+        case "LOSE":
+            computerScore++;
+            humanScore.innerText = playerScore;
+            compScore.innerText = computerScore;
+            break;
+        case "DRAW":
+            result.innerText = "It's a Draw";
+            break;
+    }
+}
+
+function firsttofive(playerSelection){
+    
+    updatescore(playerSelection);
+
+    if(playerScore == 5 || computerScore == 5){
+
+        if(playerScore > computerScore){
+            result.innertext = "You Win Human!";    
+        }
+        else if(playerScore < computerScore){
+            result.innerText = "You Lost Human!";
+            
+        }
+
+        else if(playerScore == computerScore){
+            result.innerText = "It's a Draw!";
+            
+        }
+
+    playerScore = 0;
+    computerScore = 0;
+    }
+    
+}
+
+rockBtn.addEventListener("click", function(){
+    game("rock");
+    firsttofive("rock");
+})
+
+paperBtn.addEventListener("click", function(){
+    game("paper");
+    firsttofive("paper");
+})
+
+scissorBtn.addEventListener("click", function(){
+    game("scissor");
+    firsttofive("scissor");
+})
 
 
-const rockBtn = document.querySelector('.rock');
-
-rockBtn.addEventListener('click', playRound);
